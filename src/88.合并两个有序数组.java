1868fb023fd9c2e020df7e745cee7b36b93e5670
EinSamstorch <einsamstorch@gmail.com>
2020-09-17 14:38:11 +0800

    add: 88.合并两个有序数组
    1.直接用合并两个数组，然后排序，但是这种方法没有用到两个数组本来就是有序的特性
    2.三指针，从前往后，会破坏nums1本身，所以需要提前拷贝
    3.三指针，从后往前，不需要提前拷贝。

---
+++ src/88.合并两个有序数组.java
@@ -0,0 +1,97 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=88 lang=java
+ *
+ * [88] 合并两个有序数组
+ *
+ * https://leetcode-cn.com/problems/merge-sorted-array/description/
+ *
+ * algorithms
+ * Easy (48.57%)
+ * Likes:    626
+ * Dislikes: 0
+ * Total Accepted:    207.6K
+ * Total Submissions: 426.8K
+ * Testcase Example:  '[1,2,3,0,0,0]\n3\n[2,5,6]\n3'
+ *
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+ *
+ *
+ *
+ * 说明:
+ *
+ *
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+ * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ *
+ *
+ *
+ *
+ * 示例:
+ *
+ * 输入:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ *
+ * 输出: [1,2,2,3,5,6]
+ *
+ */
+
+// @lc code=start
+class Solution {
+    /**
+     * 合并数组，排序
+     */
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+    }
+
+    /**
+     * 三指针，从前往后
+     */
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        // Make a copy of nums1。从头改变nums1时会破坏原来的数据，所以需要复制
+        int[] nums1_copy = Arrays.copyOf(nums1, m);
+        // 给nums1_copy和nums2的两个指针
+        int p1 = 0;
+        int p2= 0;
+        // 给nums1的指针
+        int p = 0;
+
+        // Compare elements from nums1_copy and nums2
+        // and add the smallest one into nums1
+        while (p1 < m && p2 < n) {
+            nums1[p++] = (nums1_copy[p1] < nums2[p2]) ? nums1_copy[p1++] : nums2[p2++];
+        }
+
+        // if there are still elements to add
+        if (p1 < m) {
+            System.arraycopy(nums1_copy, p1, nums1, p1 + p2, m + n - p1 -p2);
+        } 
+        if (p2 < n) {
+            System.arraycopy(nums2, p2, nums1, p1 + p2, m + n - p1 - p2);
+        }
+
+    }
+
+    /**
+     * 三指针，从后往前
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // two get pointers for nums1 and nums2
+        int p1 = m - 1;
+        int p2 = n - 1;
+        // set pointer for nums1
+        int p = m + n - 1;
+
+        while (p1 >= 0 && p2 >= 0) {
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+        }
+
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+    }
+}
+// @lc code=end
+
