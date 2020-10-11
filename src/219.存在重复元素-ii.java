76ad9ce603d84d25393cf968f77485d48aa5a5da
EinSamstorch <einsamstorch@gmail.com>
2020-10-11 11:38:08 +0800

    add : 217.存在重复的元素 && 219.存在重复的元素2

---
+++ src/219.存在重复元素-ii.java
@@ -0,0 +1,56 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=219 lang=java
+ *
+ * [219] 存在重复元素 II
+ *
+ * https://leetcode-cn.com/problems/contains-duplicate-ii/description/
+ *
+ * algorithms
+ * Easy (40.45%)
+ * Likes:    207
+ * Dislikes: 0
+ * Total Accepted:    63.5K
+ * Total Submissions: 157K
+ * Testcase Example:  '[1,2,3,1]\n3'
+ *
+ * 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的
+ * 绝对值 至多为 k。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums = [1,2,3,1], k = 3
+ * 输出: true
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums = [1,0,1,1], k = 1
+ * 输出: true
+ * 
+ * 示例 3:
+ * 
+ * 输入: nums = [1,2,3,1,2,3], k = 2z
+ * 输出: false
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // HashMap里不能放重复的key
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) != null && Math.abs(map.get(nums[i]) - i) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
+// @lc code=end
+
