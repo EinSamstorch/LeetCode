98244b6fd7b23009c170535e6a0e4a8e62f22fa9
EinSamstorch <einsamstorch@gmail.com>
2020-10-13 23:36:34 +0800

    add: 414.第三大的数.java

---
+++ src/414.第三大的数.java
@@ -0,0 +1,99 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+/*
+ * @lc app=leetcode.cn id=414 lang=java
+ *
+ * [414] 第三大的数
+ *
+ * https://leetcode-cn.com/problems/third-maximum-number/description/
+ *
+ * algorithms
+ * Easy (35.47%)
+ * Likes:    173
+ * Dislikes: 0
+ * Total Accepted:    36.4K
+ * Total Submissions: 102.6K
+ * Testcase Example:  '[3,2,1]'
+ *
+ * 给定一个非空数组，返回此数组中第三大的数。如果不存在，则返回数组中最大的数。要求算法时间复杂度必须是O(n)。
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: [3, 2, 1]
+ *
+ * 输出: 1
+ *
+ * 解释: 第三大的数是 1.
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: [1, 2]
+ *
+ * 输出: 2
+ *
+ * 解释: 第三大的数不存在, 所以返回最大的数 2 .
+ *
+ *
+ * 示例 3:
+ *
+ *
+ * 输入: [2, 2, 3, 1]
+ *
+ * 输出: 1
+ *
+ * 解释: 注意，要求返回第三大的数，是指第三大且唯一出现的数。
+ * 存在两个值为2的数，它们都排第二。
+ *
+ *
+ */
+
+// @lc code=start
+class Solution {
+    public int thirdMax1(int[] nums) {
+        long first = Long.MIN_VALUE;
+        long second = Long.MIN_VALUE;
+        long third = Long.MIN_VALUE;
+        for (int num : nums) {
+            // 排除一样得数
+            if (num == first || num == second || num == third) {
+                continue;
+            }
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (num > second) {
+                third = second;
+                second = num;
+            } else if (num > third) {
+                third = num;
+            }
+        }
+        if (third != Long.MIN_VALUE) {
+            return (int) third;
+        } else {
+            return (int) first;
+        }
+    }
+
+    public int thirdMax(int[] nums) {
+        // treeset是一个有序的集合，存放数字时默认按升序排序，
+        // 且不能存放重复的数字
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int num : nums) {
+            set.add(num);
+            if (set.size() > 3) {
+                set.remove(set.first());
+            }
+        }
+        return set.size() < 3 ? set.last() : set.first();
+    }
+
+}
+// @lc code=end
+
