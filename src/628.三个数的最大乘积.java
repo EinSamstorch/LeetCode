47ab2317b8ffd02370def7fa70e5ed8ee5a8a064
EinSamstorch <einsamstorch@gmail.com>
2020-11-18 22:55:59 +0800

    add： 628.三个数的最大乘积
    1. 暴力算法
    2. 线性扫描法，自己找出最大的三个数和最小的两个数

---
+++ src/628.三个数的最大乘积.java
@@ -0,0 +1,85 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=628 lang=java
+ *
+ * [628] 三个数的最大乘积
+ *
+ * https://leetcode-cn.com/problems/maximum-product-of-three-numbers/description/
+ *
+ * algorithms
+ * Easy (50.09%)
+ * Likes:    191
+ * Dislikes: 0
+ * Total Accepted:    32.6K
+ * Total Submissions: 65.1K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个整型数组，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: [1,2,3]
+ * 输出: 6
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: [1,2,3,4]
+ * 输出: 24
+ * 
+ * 
+ * 注意:
+ * 
+ * 
+ * 给定的整型数组长度范围是[3,10^4]，数组中所有的元素范围是[-1000, 1000]。
+ * 输入的数组中任意三个数的乘积不会超出32位有符号整数的范围。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int maximumProduct1(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return Math.max(nums[n - 1] * nums[n - 2] * nums[n - 3], nums[n - 1] * nums[0] * nums[1]);
+    }
+
+    /**
+     * 找出最大的三个数和最小的两个数
+     * 不排序，线性扫描
+     */
+    public int maximumProduct(int[] nums) {
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+
+        for (int n : nums) {
+            if (n <= min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n <= min2) {
+                min2 = n;
+            }
+            // n is greater than max1, max2, max3
+            if (n >= max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            // n lies between max1 and max2
+            } else if (n >= max2) {
+                max3 = max2;
+                max2 = n;
+            // n lies between max2 and max3
+            } else if (n >= max3) {
+                max3 = n;
+            }
+        }
+        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
+        
+    }
+}
+// @lc code=end
+
