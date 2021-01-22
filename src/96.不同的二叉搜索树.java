1d3e00a494fe96cf7f1b6d3db3f3743b419bf5e4
EinSamstorch <einsamstorch@gmail.com>
2021-01-22 22:17:40 +0800

    add: 96.不同的二叉搜索树.java
    动态规划的方法

--- src/96.不同的二叉搜索树.java
+++ src/96.不同的二叉搜索树.java
@@ -35,11 +35,11 @@ class Solution {
     public int numTrees(int n) {
         int[] ans = new int[n + 1];
         ans[0] = 1;
-        ans[1] = 0;
+        ans[1] = 1;
         // 当i是根节点时，其左子树的节点为i - 1， 右子树的节点为n - i
         for (int i = 2; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                ans[i] = ans[j - 1] * ans[i - j];
+                ans[i] += ans[j - 1] * ans[i - j];
             }
         }
         return ans[n];
