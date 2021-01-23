9d12a54bf66c5d6a3e6db609d67f1484af733b1e
EinSamstorch <einsamstorch@gmail.com>
2021-01-23 13:53:04 +0800

    add: 95.不同的二叉搜索树-ii
    递归的方法
    中序遍历

---
+++ src/95.不同的二叉搜索树-ii.java
@@ -0,0 +1,107 @@
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=95 lang=java
+ *
+ * [95] 不同的二叉搜索树 II
+ *
+ * https://leetcode-cn.com/problems/unique-binary-search-trees-ii/description/
+ *
+ * algorithms
+ * Medium (66.83%)
+ * Likes:    761
+ * Dislikes: 0
+ * Total Accepted:    71K
+ * Total Submissions: 106.2K
+ * Testcase Example:  '3'
+ *
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入：3
+ * 输出：
+ * [
+ * [1,null,3,2],
+ * [3,2,null,1],
+ * [3,1,null,null,2],
+ * [2,1,3],
+ * [1,null,2,null,3]
+ * ]
+ * 解释：
+ * 以上的输出对应以下 5 种不同结构的二叉搜索树：
+ * 
+ * ⁠  1         3     3      2      1
+ * ⁠   \       /     /      / \      \
+ * ⁠    3     2     1      1   3      2
+ * ⁠   /     /       \                 \
+ * ⁠  2     1         2                 3
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 <= n <= 8
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generateTrees(1, n);
+    }
+
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+
+        // 枚举可行根节点
+        for (int i = start; i <= end; i++) {
+            // 获得所有可行的左子树集合
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+            // 获取所有可行的右子树集合
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+            // 从左子树集合中选出一颗左子树，从右子树集合中选出一颗右子树，拼接到根节点上
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right: rightTrees) {
+                    TreeNode currTree = new TreeNode(i);
+                    currTree.left = left;
+                    currTree.right = right;
+                    allTrees.add(currTree);
+                }
+            }
+        }
+        return allTrees;
+    }
+}
+// @lc code=end
+
