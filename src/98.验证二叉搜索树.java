e43c76f9911cf58bfc27f80b528687d7d5503dfa
EinSamstorch <einsamstorch@gmail.com>
2021-01-23 14:06:10 +0800

    add：98.验证二叉搜索树
    1. 递归的方法
    2. 使用栈的中序遍历

---
+++ src/98.验证二叉搜索树.java
@@ -0,0 +1,127 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=98 lang=java
+ *
+ * [98] 验证二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (33.30%)
+ * Likes:    895
+ * Dislikes: 0
+ * Total Accepted:    213.1K
+ * Total Submissions: 639.9K
+ * Testcase Example:  '[2,1,3]'
+ *
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * 
+ * 假设一个二叉搜索树具有如下特征：
+ * 
+ * 
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 1   4
+ * / \
+ * 3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ * 根节点的值为 5 ，但是其右子节点值为 4 。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * 递归的方法
+     */
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+
+    public boolean helper(TreeNode node, Integer lower, Integer upper) {
+        if (node == null) {
+            return true;
+        }
+        int val = node.val;
+        if (lower != null && val <= lower) {
+            return false;
+        }
+        if (upper != null && val >= upper) {
+            return false;
+        }
+        // 递归调用右子树的时候，我们需要把下界lower改为root.val 
+        if (!helper(node.right, val, upper)) {
+            return false;
+        }
+        // 递归调用左子树的时候， 将上界upper改为root.val
+        if (!helper(node.left, lower, val)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 根据二叉搜索树的性质，中序遍历的结果是单调递增的
+     * @param root
+     */
+    public isValidBST(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        double inorder = -Double.MAX_VALUE;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            // 如果中序遍历得到的节点的值小于等于前一个inorder，说明不是二叉搜索树
+            if (root.val <= inorder) {
+                return false;
+            }
+            inorder = root.val;
+            root = root.right;
+        }
+    }
+
+
+}
+// @lc code=end
+
