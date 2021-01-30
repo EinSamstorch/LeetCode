7ecce81bf51f9065808e9581e9d613a97dbcfa5b
einsamstorch <einsamstorch@qq.com>
2021-01-30 09:59:49 +0800

    add：5.最长回文子串
    使用中心扩展法

---
+++ src/516.最长回文子序列.java
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode.cn id=516 lang=java
+ *
+ * [516] 最长回文子序列
+ *
+ * https://leetcode-cn.com/problems/longest-palindromic-subsequence/description/
+ *
+ * algorithms
+ * Medium (59.32%)
+ * Likes:    373
+ * Dislikes: 0
+ * Total Accepted:    36.9K
+ * Total Submissions: 62.2K
+ * Testcase Example:  '"bbbab"'
+ *
+ * 给定一个字符串 s ，找到其中最长的回文子序列，并返回该序列的长度。可以假设 s 的最大长度为 1000 。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 输入:
+ * 
+ * "bbbab"
+ * 
+ * 
+ * 输出:
+ * 
+ * 4
+ * 
+ * 
+ * 一个可能的最长回文子序列为 "bbbb"。
+ * 
+ * 示例 2:
+ * 输入:
+ * 
+ * "cbbd"
+ * 
+ * 
+ * 输出:
+ * 
+ * 2
+ * 
+ * 
+ * 一个可能的最长回文子序列为 "bb"。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= s.length <= 1000
+ * s 只包含小写英文字母
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        // dp[i][j] 表示s[i...j]的最长回文子序列
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        // 反着遍历保证正确的状态转移
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    // 如果他两不相等，他两不可能同时出现在s[i...j]的最长回文子序列中
+                    // 将他两分别加入
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
+// @lc code=end
+
