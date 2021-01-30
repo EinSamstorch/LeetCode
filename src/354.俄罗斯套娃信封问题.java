7a90b09e4b1b8d498c7eb77b81b72e4b6b07afbf
einsamstorch <einsamstorch@qq.com>
2021-01-30 09:39:18 +0800

    add：354.俄罗斯套娃问题
    排序+300.最长递增子序列

---
+++ src/354.俄罗斯套娃信封问题.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+/*
+ * @lc app=leetcode.cn id=354 lang=java
+ *
+ * [354] 俄罗斯套娃信封问题
+ *
+ * https://leetcode-cn.com/problems/russian-doll-envelopes/description/
+ *
+ * algorithms
+ * Hard (38.66%)
+ * Likes:    265
+ * Dislikes: 0
+ * Total Accepted:    23.7K
+ * Total Submissions: 61.2K
+ * Testcase Example:  '[[5,4],[6,4],[6,7],[2,3]]'
+ *
+ * 给定一些标记了宽度和高度的信封，宽度和高度以整数对形式 (w, h)
+ * 出现。当另一个信封的宽度和高度都比这个信封大的时候，这个信封就可以放进另一个信封里，如同俄罗斯套娃一样。
+ * 
+ * 请计算最多能有多少个信封能组成一组“俄罗斯套娃”信封（即可以把一个信封放到另一个信封里面）。
+ * 
+ * 说明:
+ * 不允许旋转信封。
+ * 
+ * 示例:
+ * 
+ * 输入: envelopes = [[5,4],[6,4],[6,7],[2,3]]
+ * 输出: 3 
+ * 解释: 最多信封的个数为 3, 组合为: [2,3] => [5,4] => [6,7]。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        int n = envelopes.length;
+        // 按宽度升序排列，如果宽度一样，则按照高度降序排列
+        // 之所以按照高度降序，是因为宽度相同的信封也不能嵌套
+        Arrays.sort(envelopes, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                return a[0] == b[0] ?
+                    b[1] - a[1] : a[0] - b[0];
+            }
+        });
+        int[] height = new int[n];
+        for (int i = 0; i < n; i++) {
+            height[i] = envelopes[i][1];
+        }
+        // 再对height数组使用最长递增子序列问题就好
+        return lengthOfLIS(height);
+    }
+
+    private int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int res = 0; 
+        for (int i = 0; i < dp.length; i++) {
+            res = Math.max(dp[i], res);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
