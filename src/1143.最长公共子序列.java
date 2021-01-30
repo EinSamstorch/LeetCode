c912feb611a81538c3ad99a9179d05ed77b5f87f
einsamstorch <einsamstorch@qq.com>
2021-01-30 09:37:54 +0800

    add：1143.最长公共子序列

---
+++ src/1143.最长公共子序列.java
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=java
+ *
+ * [1143] 最长公共子序列
+ *
+ * https://leetcode-cn.com/problems/longest-common-subsequence/description/
+ *
+ * algorithms
+ * Medium (60.81%)
+ * Likes:    351
+ * Dislikes: 0
+ * Total Accepted:    65.5K
+ * Total Submissions: 107.6K
+ * Testcase Example:  '"abcde"\n"ace"'
+ *
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
+ * 
+ * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde"
+ * 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+ * 
+ * 若这两个字符串没有公共子序列，则返回 0。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入：text1 = "abcde", text2 = "ace" 
+ * 输出：3  
+ * 解释：最长公共子序列是 "ace"，它的长度为 3。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入：text1 = "abc", text2 = "abc"
+ * 输出：3
+ * 解释：最长公共子序列是 "abc"，它的长度为 3。
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入：text1 = "abc", text2 = "def"
+ * 输出：0
+ * 解释：两个字符串没有公共子序列，返回 0。
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 <= text1.length <= 1000
+ * 1 <= text2.length <= 1000
+ * 输入的字符串只含有小写英文字符。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length() + 1;
+        int len2 = text2.length() + 1;
+        // 对于s1[1..i]和s2[1..j]，它们的 LCS 长度是dp[i][j]。
+        // 由于我们认为索引是从1开始，所以代码中需要调整下长度
+        int[][] dp = new int[len1][len2];
+
+        for (int i = 1; i < len1; i++) {
+            for (int j = 1; j < len2; j++) {
+                // 从前往后遍历，第i个元素就是索引i - 1
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    // 谁能让lcs更长，就听谁的
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[len1 - 1][len2 - 1];
+    }
+}
+// @lc code=end
+
