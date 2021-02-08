9c5c086958af187c10c25ae1e81f5d024dad99b7
einsamstorch <einsamstorch@qq.com>
2021-02-08 14:08:16 +0800

    add: 312.戳气球

---
+++ src/312.戳气球.java
@@ -0,0 +1,81 @@
+/*
+ * @lc app=leetcode.cn id=312 lang=java
+ *
+ * [312] 戳气球
+ *
+ * https://leetcode-cn.com/problems/burst-balloons/description/
+ *
+ * algorithms
+ * Hard (67.57%)
+ * Likes:    623
+ * Dislikes: 0
+ * Total Accepted:    37.8K
+ * Total Submissions: 55.9K
+ * Testcase Example:  '[3,1,5,8]'
+ *
+ * 有 n 个气球，编号为0 到 n - 1，每个气球上都标有一个数字，这些数字存在数组 nums 中。
+ * 
+ * 现在要求你戳破所有的气球。戳破第 i 个气球，你可以获得 nums[i - 1] * nums[i] * nums[i + 1] 枚硬币。 这里的 i
+ * - 1 和 i + 1 代表和 i 相邻的两个气球的序号。如果 i - 1或 i + 1 超出了数组的边界，那么就当它是一个数字为 1 的气球。
+ * 
+ * 求所能获得硬币的最大数量。
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [3,1,5,8]
+ * 输出：167
+ * 解释：
+ * nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+ * coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [1,5]
+ * 输出：10
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * n == nums.length
+ * 1 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        // 添加两侧的虚拟气球
+        int[] points = new int[n + 2];
+        points[0] = points[n + 1] = 1;
+        for (int i = 1; i <= n; i++) {
+            points[i] = nums[i - 1];
+        }
+        // base case 已经都被初始化为0
+        // dp[i][j] = x表示，戳破气球i和气球j之间（开区间，不包括i和j）的所有气球，可以获得的最高分数为x。
+        int[][] dp = new int[n + 2][n + 2];
+        // 开始状态转移
+        // i应该从下往上
+        for (int i = n; i >= 0; i--) {
+            // j应该从左到右
+            for (int j = i + 1; j < n + 2; j++) {
+                // 戳破的气球是哪一个
+                for (int k = i + 1;  k < j; k++) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i][k] + dp[k][j] + points[i] * points[j] * points[k]);    
+                }
+            }
+        }
+        return dp[0][n + 1];
+
+    }
+}
+// @lc code=end
+
