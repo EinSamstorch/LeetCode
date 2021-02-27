5349edc8f125293e4f5e217676de709dd439084a
EinSamstorch <einsamstorch@gmail.com>
2021-02-27 23:32:14 +0800

    add: 451.根据字符出现频率排序
    高级排序

---
+++ src/451.根据字符出现频率排序.java
@@ -0,0 +1,96 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+/*
+ * @lc app=leetcode.cn id=451 lang=java
+ *
+ * [451] 根据字符出现频率排序
+ *
+ * https://leetcode-cn.com/problems/sort-characters-by-frequency/description/
+ *
+ * algorithms
+ * Medium (66.61%)
+ * Likes:    220
+ * Dislikes: 0
+ * Total Accepted:    39.2K
+ * Total Submissions: 58.9K
+ * Testcase Example:  '"tree"'
+ *
+ * 给定一个字符串，请将字符串里的字符按照出现的频率降序排列。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入:
+ * "tree"
+ * 
+ * 输出:
+ * "eert"
+ * 
+ * 解释:
+ * 'e'出现两次，'r'和't'都只出现一次。
+ * 因此'e'必须出现在'r'和't'之前。此外，"eetr"也是一个有效的答案。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入:
+ * "cccaaa"
+ * 
+ * 输出:
+ * "cccaaa"
+ * 
+ * 解释:
+ * 'c'和'a'都出现三次。此外，"aaaccc"也是有效的答案。
+ * 注意"cacaca"是不正确的，因为相同的字母必须放在一起。
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 
+ * 输入:
+ * "Aabb"
+ * 
+ * 输出:
+ * "bbAa"
+ * 
+ * 解释:
+ * 此外，"bbaA"也是一个有效的答案，但"Aabb"是不正确的。
+ * 注意'A'和'a'被认为是两种不同的字符。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    /**
+     * map统计次数 + 优先队列
+     * @param s
+     * @return
+     */
+    public String frequencySort(String s) {
+        char[] chars = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : chars) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>((e1, e2) -> e2.getValue() - e1.getValue());
+
+        maxHeap.addAll(map.entrySet());
+
+        StringBuilder sortedStrings = new StringBuilder(s.length());
+        while (!maxHeap.isEmpty()) {
+            Map.Entry<Character, Integer> entry = maxHeap.poll();
+            for (int i = 0; i < entry.getValue(); i++) {
+                sortedStrings.append(entry.getKey());
+            }
+        }
+        return sortedStrings.toString();
+
+    }
+}
+// @lc code=end
+
