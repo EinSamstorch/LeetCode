356cfce18baddde67070dda4612499b134d904a1
EinSamstorch <einsamstorch@gmail.com>
2021-03-22 20:15:27 +0800

    add: 贪心

---
+++ src/55.跳跃游戏.java
@@ -0,0 +1,106 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=java
+ *
+ * [55] 跳跃游戏
+ *
+ * https://leetcode-cn.com/problems/jump-game/description/
+ *
+ * algorithms
+ * Medium (41.84%)
+ * Likes:    1082
+ * Dislikes: 0
+ * Total Accepted:    205.2K
+ * Total Submissions: 490.2K
+ * Testcase Example:  '[2,3,1,1,4]'
+ *
+ * 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+ * 
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * 
+ * 判断你是否能够到达最后一个下标。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [2,3,1,1,4]
+ * 输出：true
+ * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [3,2,1,0,4]
+ * 输出：false
+ * 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    /**
+     * 我们依次遍历数组中的每一个位置，并实时维护 最远可以到达的位置。对于当前遍历到的位置 x，如果它在 最远可以到达的位置 的范围内，那么我们就可以从起点通过若干次跳跃到达该位置，因此我们可以用x+nums[x] 更新 最远可以到达的位置。
+    在遍历的过程中，如果 最远可以到达的位置 大于等于数组中的最后一个位置，那就说明最后一个位置可达，我们就可以直接返回 True 作为答案。反之，如果在遍历结束后，最后一个位置仍然不可达，我们就返回 False 作为答案。
+     */
+    public boolean canJump1(int[] nums) {
+        int len = nums.length;
+        int rightMost = 0;
+        for (int i = 0; i < len; i++) {
+            if (i <= rightMost) {
+                rightMost = Math.max(rightMost, i + nums[i]);
+                if (rightMost >= len - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean canJump2(int[] nums) {
+        if (nums == null) {
+            return false;
+        }
+        //前n-1个元素能够跳到的最远距离
+        int k = 0;
+        for (int i = 0; i <= k; i++) {
+            //第i个元素能够跳到的最远距离
+            int temp = i + nums[i];
+            //更新最远距离
+            k = Math.max(k, temp);
+            //如果最远距离已经大于或等于最后一个元素的下标,则说明能跳过去,退出. 减少循环
+            if (k >= nums.length - 1) {
+                return true;
+            }
+        }
+        //最远距离k不再改变,且没有到末尾元素
+        return false;
+    }
+
+    public boolean canJump(int[] nums) {
+        int len = nums.length;
+        // 最远可以到达的距离
+        int rightMost = 0;
+
+        for (int i = 0; i < len; i++) {
+            if (i > rightMost) {
+                return false;
+            }
+            rightMost = Math.max(rightMost, i + nums[i]);
+        }
+        return true;
+    }
+}
+// @lc code=end
+
